446c25710fa6586f4ed9d27bc1608a28cb357308
mafecaalegria-cmd <mafecaalegria@gmail.com>
2026-02-10 16:36:06 +0000

    primer cambio

---
+++ semana2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package semana2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+         // forma.1 definiendo diretamente los valores 
+    int[] a = {4,8,9,6,1,2};
+    // recorres los elementos de riegos
+        for (int i = 0; i < a.length; i++) {
+            System.err.println("a["+i+"]="+a[i]);
+    }
+    
+}
+}
